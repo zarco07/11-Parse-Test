85a5532dd1d19cf4f32d8ddadec9fada6a92571c
Oscar Zarco <oscarzarco@iMac.local>
2015-03-05 13:18:02 -0600

    Initial Commit

---
+++ 11 Parse Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  11 Parse Test
+//
+//  Created by Oscar Zarco on 05/03/15.
+//  Copyright (c) 2015 Tenologias ZD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 11 Parse Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  11 Parse Test
+//
+//  Created by Oscar Zarco on 05/03/15.
+//  Copyright (c) 2015 Tenologias ZD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
